09a64d8a699ef9b0c5df671f4c1bfa7532f5f090
Maxi2 <maxhr117@gmail.com>
2024-03-25 20:44:49 -0600

    primer commmit de varios ejercicios simples

---
+++ calcular_area_y_perimetro.js
@@ -0,0 +1,11 @@
+//Definir numero valor de pared y numero de lados de un cuadrado
+var lado = 40
+var numeroDeLados = 4
+//Calcular el perimetro del cuadrado
+var perimetro = lado * numeroDeLados;
+//Calcular el area del cuadrado
+var area = lado * lado;
+//Mostrar resultado
+console.log("El area del cuadrado es: " + perimetro + "metros")
+console.log("El area del cuadrado es: " + area + "m^2")
+
---
+++ calcular_precio_con_iva.js
@@ -0,0 +1,11 @@
+//Definir precio e Iva
+var Precio = 200
+var Iva = 21
+
+//Calcular el precio total
+var precioTotal = Precio+ (Precio * Iva / 100);
+
+//Mostrar resultado
+console.log("El precio total de la compra es: " + precioTotal + "$")
+
+
